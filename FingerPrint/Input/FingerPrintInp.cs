f7b83014e5a199e60df2a1f3756ef17d89757ecb
Ali Hamza Ansari <69114218+Ali-Hamza-An5ari@users.noreply.github.com>
2023-04-08 13:12:12 +0500

    FingerPrint models

---
+++ FingerPrint/Input/FingerPrintInp.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Entities.Model.FingerPrint.Input;
+
+public class FingerPrintInp
+{
+    [JsonIgnore]
+    public Guid Id { get; set; }
+    [Required]
+    public Guid EmployeeId { get; set; }
+    [Required]
+    public byte[] FingerPrintData { get; set; }
+}
---
+++ FingerPrint/View/FingerPrint.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.Model.FingerPrint.View;
+
+public class FingerPrint
+{
+    public Guid Id { get; set; } = Guid.Empty;
+    public Guid EmployeeId { get; set; } = Guid.Empty;
+    public byte[] FingerPrintData { get; set; } = new byte[1024];
+}
