bfb0e6b3edd941cd536a3bbca544dd4e321df17d
Ali Hamza Ansari <69114218+Ali-Hamza-An5ari@users.noreply.github.com>
2023-04-08 13:10:37 +0500

    Service

---
+++ FingerPrintService.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Model.FingerPrint.Input;
+using Entities.Model.FingerPrint.View;
+using KonnectApiv4.Application.IRepository;
+using KonnectApiv4.Application.IService;
+
+namespace KonnectApiv4.Infrastructure.Service;
+
+public class FingerPrintService: IFingerPrintService
+{
+    private readonly IFingerPrintRepo _fingerPrintRepo;
+
+    public FingerPrintService(IFingerPrintRepo fingerPrintRepo)
+    {
+        _fingerPrintRepo = fingerPrintRepo;
+    }
+
+    public Task<FingerPrint> AddFingerPrintAsync(FingerPrintInp model)
+    {
+        return _fingerPrintRepo.AddFingerPrintAsync(model);
+    }
+
+    public Task<IEnumerable<FingerPrint>> GetAllFingerPrintAsync()
+    {
+        return _fingerPrintRepo.GetAllFingerPrintAsync();
+    }
+}
