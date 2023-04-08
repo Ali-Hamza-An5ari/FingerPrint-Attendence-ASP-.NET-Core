5b24c16c0ca57581dbf6594d590b7d15ac54c794
Ali Hamza Ansari <69114218+Ali-Hamza-An5ari@users.noreply.github.com>
2023-04-08 13:09:47 +0500

    IService

---
+++ IFingerPrintService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Model.FingerPrint.Input;
+using Entities.Model.FingerPrint.View;
+
+namespace KonnectApiv4.Application.IService;
+
+public interface IFingerPrintService
+{
+    Task<FingerPrint> AddFingerPrintAsync(FingerPrintInp model);
+    // Task<FingerPrint> GetFingerPrintByIdAsync(Guid fingerPrintId);
+    Task<IEnumerable<FingerPrint>> GetAllFingerPrintAsync();
+    // Task<FingerPrint> GetFingerPrintByEmployeeAsync(Guid employeeId);
+    // Task<FingerPrint> UpdateFingerPrintAsync(FingerPrintInp model);
+    // Task<bool> RemoveFingerPrintAsync(Guid fingerPrintId);
+}
